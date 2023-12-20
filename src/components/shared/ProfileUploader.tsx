5b080fae559968b032b19bb4b95416f60446a234
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-20 17:42:15 +0100

    NewFix

--- src/components/shared/ProfileUploader.tsx
+++ src/components/shared/ProfileUploader.tsx
@@ -1,8 +1,16 @@
 import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
-
+import EXIF from "exif-js";
 import { convertFileToUrl } from "@/lib/utils";
 
+// Add the BinaryFile declaration here
+declare class BinaryFile {
+    constructor(data: ArrayBuffer);
+    readonly data: ArrayBuffer;
+    readonly asDataView: DataView;
+    readonly asBinaryString: string;
+    readonly asArrayBuffer: ArrayBuffer;
+}
 
 type ProfileUploaderProps = {
     fieldChange: (files: File[]) => void;
@@ -15,9 +23,42 @@ const ProfileUploader = ({ fieldChange, mediaUrl }: ProfileUploaderProps) => {
 
     const onDrop = useCallback(
         (acceptedFiles: FileWithPath[]) => {
-            setFile(acceptedFiles);
-            fieldChange(acceptedFiles);
-            setFileUrl(convertFileToUrl(acceptedFiles[0]));
+            // Assuming only one file is dropped
+            const droppedFile = acceptedFiles[0];
+
+            const fileReader = new FileReader();
+            fileReader.onloadend = function (event) {
+                const result = event.target?.result; // Type of result is 'string | ArrayBuffer | null'
+
+                if (result instanceof ArrayBuffer) {
+                    const exif = EXIF.readFromBinaryFile(new BinaryFile(result));
+
+                    // Your EXIF orientation switch logic
+                    switch (exif.Orientation) {
+                        case 8:
+                            // Rotate 90 degrees
+                            break;
+                        case 3:
+                            // Rotate 180 degrees
+                            break;
+                        case 6:
+                            // Rotate -90 degrees
+                            break;
+                        // Add more cases if needed
+                    }
+
+                    // Continue with your existing logic
+                    // For example, you might want to upload the rotated file or display it.
+                    setFile(acceptedFiles);
+                    fieldChange(acceptedFiles);
+                    setFileUrl(convertFileToUrl(droppedFile));
+                } else {
+                    console.error("FileReader onloadend: Unexpected result type");
+                }
+            };
+
+            // Start reading the file as an ArrayBuffer
+            fileReader.readAsArrayBuffer(droppedFile);
         },
         [file]
     );
