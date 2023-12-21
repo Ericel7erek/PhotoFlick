b13e926352d9920e90b2b0ac0419e8cc56a7317c
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-21 13:20:14 +0100

    new

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import AuthProvider from "./context/AuthContext";
 import QueryProvider from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <div className="overflow-hidden overscroll-none scroll-smooth">
+    <div className="">
         <BrowserRouter>
             <QueryProvider>
                 <AuthProvider>
