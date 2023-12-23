fb2a1a8e801e46b0f9f51eabb8b54d072f8304e1
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-23 15:58:12 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "PhotoFlick", // Replace 'your-app-name' with the actual name of your app
+  base: `"/"`, // Replace 'your-app-name' with the actual name of your app
   plugins: [react()],
   resolve: {
     alias: {
