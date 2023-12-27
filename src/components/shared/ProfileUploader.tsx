9206f57e9aa095161dbef7fa510ff795dd3e192d
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-27 16:48:24 +0100

    revert

--- src/components/shared/ProfileUploader.tsx
+++ src/components/shared/ProfileUploader.tsx
@@ -2,9 +2,6 @@ import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
 import { convertFileToUrl } from "@/lib/utils";
 
-// Import the removeExifOrientation function
-import { removeExifOrientation } from "@/lib/utils";
-
 type ProfileUploaderProps = {
     fieldChange: (files: File[]) => void;
     mediaUrl: string;
@@ -14,32 +11,13 @@ const ProfileUploader = ({ fieldChange, mediaUrl }: ProfileUploaderProps) => {
     const [file, setFile] = useState<File[]>([]);
     const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
 
-    // Function to handle Exif orientation before uploading
-    const handleUpload = async (file: File) => {
-        // Remove Exif orientation metadata before uploading
-        const fileWithoutExif = await removeExifOrientation(file);
-
-        // Now you can proceed with uploading fileWithoutExif
-        // Your actual upload logic goes here
-        // ...
-
-        // For now, let's log the modified file URL
-        console.log("File URL after removing Exif orientation:", convertFileToUrl(fileWithoutExif));
-    };
-
     const onDrop = useCallback(
-        async (acceptedFiles: FileWithPath[]) => {
-            const selectedFile = acceptedFiles[0];
-
-            // Handle Exif orientation before uploading
-            await handleUpload(selectedFile);
-
-            // Rest of your logic goes here
+        (acceptedFiles: FileWithPath[]) => {
             setFile(acceptedFiles);
             fieldChange(acceptedFiles);
             setFileUrl(convertFileToUrl(acceptedFiles[0]));
         },
-        [file, fieldChange]
+        [file]
     );
 
     const { getRootProps, getInputProps } = useDropzone({
